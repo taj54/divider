3ad3a8d528244c8632a1d110274311ccf634a28b
nyaomaru <nyaonyao0725@gmail.com>
2025-02-23 21:48:27 +0900

    add: string[] handle

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,110 @@
+// export function divider(
+//   input: string | string[],
+//   ...args: (number | string)[]
+// ): string[] | string[][] {
+//   //Extract the options from the input
+//   const lastArg = args[args.length - 1];
+//   const separators = args as (number | string)[];
+
+//   if (Array.isArray(input)) {
+//     const result = input.map((item) =>
+//       divider(item, ...separators)
+//     ) as string[][];
+//     return result;
+//   }
+
+//   // Deal with edge cases
+//   if (typeof input !== 'string' || separators.length === 0) {
+//     return [input];
+//   }
+
+//   // Classify number and string delimiters
+//   let numSeparators: number[] = [];
+//   let strSeparators: string[] = [];
+
+//   for (const separator of separators) {
+//     if (typeof separator === 'number') {
+//       numSeparators.push(separator);
+//     } else if (typeof separator === 'string') {
+//       strSeparators.push(separator);
+//     }
+//   }
+
+//   // Divide by string delimiters and memorize the starting position
+//   const parts: string[] = [];
+//   const startIndices: number[] = [];
+//   let currentIndex = 0;
+
+//   input
+//     .split(new RegExp(`[${strSeparators.join('')}]`, 'g'))
+//     .forEach((part) => {
+//       if (part) {
+//         parts.push(part);
+//         startIndices.push(currentIndex);
+//       }
+//       currentIndex += part.length + 1;
+//     });
+
+//   // Divide by number delimiters
+//   const sortedNumSeparators = [...new Set(numSeparators)].sort((a, b) => a - b);
+
+//   let finalParts: string[] = [];
+
+//   parts.forEach((part, i) => {
+//     const partStart = startIndices[i];
+//     let prevIndex = 0;
+//     const localSeparators = sortedNumSeparators
+//       .filter((num) => num > partStart && num < partStart + part.length)
+//       .map((num) => num - partStart);
+
+//     for (const index of localSeparators) {
+//       finalParts.push(part.slice(prevIndex, index));
+//       prevIndex = index;
+//     }
+//     finalParts.push(part.slice(prevIndex));
+//   });
+
+//   return finalParts;
+// }
+
 export function divider(
+  input: string | string[],
+  ...args: (number | string | { flatten?: boolean })[]
+): string[] | string[][] {
+  // extract the options from the input
+  const lastArg = args[args.length - 1];
+  const options =
+    typeof lastArg === 'object' ? (args.pop() as { flatten?: boolean }) : {};
+  const separators = args as (number | string)[];
+
+  if (typeof input === 'string') {
+    return divideString(input, separators);
+  }
+
+  const result = input.map((item) => divideString(item, separators));
+  return options.flatten ? result.flat() : result;
+}
+
+function divideString(
   input: string,
-  ...separators: (number | string)[]
+  separators: (number | string)[]
 ): string[] {
-  if (typeof input !== 'string' || separators.length === 0) {
+  if (separators.length === 0) {
     return [input];
   }
 
   let numSeparators: number[] = [];
   let strSeparators: string[] = [];
 
-  // Classify number and string delimiters
   for (const separator of separators) {
     if (typeof separator === 'number') {
       numSeparators.push(separator);
-    } else {
+    } else if (typeof separator === 'string') {
       strSeparators.push(separator);
     }
   }
 
-  // Divide by string delimiters and memorize the starting position
+  // Divide by string delimiters
   let parts: string[] = [];
   let startIndices: number[] = [];
   let currentIndex = 0;
@@ -26,7 +112,6 @@ export function divider(
   input
     .split(new RegExp(`[${strSeparators.join('')}]`, 'g'))
     .forEach((part) => {
-      console.log('part', part);
       if (part) {
         parts.push(part);
         startIndices.push(currentIndex);
@@ -35,14 +120,14 @@ export function divider(
     });
 
   // Divide by number delimiters
-  numSeparators = [...new Set(numSeparators)].sort((a, b) => a - b);
+  const sortedNumSeparators = [...new Set(numSeparators)].sort((a, b) => a - b);
   let finalParts: string[] = [];
 
   for (let i = 0; i < parts.length; i++) {
-    let part = parts[i];
-    let partStart = startIndices[i];
+    const part = parts[i];
+    const partStart = startIndices[i];
     let prevIndex = 0;
-    let localSeparators = numSeparators
+    const localSeparators = sortedNumSeparators
       .filter((num) => num > partStart && num < partStart + part.length)
       .map((num) => num - partStart);
 
--- tests/divider.test.ts
+++ tests/divider.test.ts
@@ -1,6 +1,6 @@
 import { divider } from '../src/index';
 
-describe('divider', () => {
+describe('divider with string', () => {
   test('divide with single number', () => {
     expect(divider('hello', 2)).toEqual(['he', 'llo']);
   });
@@ -37,5 +37,84 @@ describe('divider', () => {
     expect(divider('hello', 0)).toEqual(['hello']);
     expect(divider('hello', 5)).toEqual(['hello']);
     expect(divider('hello', 10)).toEqual(['hello']);
+  });
+});
+
+describe('divider with string[]', () => {
+  test('divide with single number', () => {
+    expect(divider(['hello', 'world'], 2)).toEqual([
+      ['he', 'llo'],
+      ['wo', 'rld'],
+    ]);
+  });
+
+  test('divide with multiple number', () => {
+    expect(divider(['hello', 'new world'], 1, 3)).toEqual([
+      ['h', 'el', 'lo'],
+      ['n', 'ew', ' world'],
+    ]);
+    expect(divider(['hello', 'new world'], 1, 3, 4)).toEqual([
+      ['h', 'el', 'l', 'o'],
+      ['n', 'ew', ' ', 'world'],
+    ]);
+    expect(divider(['hello', 'new world'], 1, 3, 5)).toEqual([
+      ['h', 'el', 'lo'],
+      ['n', 'ew', ' w', 'orld'],
+    ]);
+  });
+
+  test('divide with reverse number', () => {
+    expect(divider(['hello', 'new world'], 2, 1)).toEqual([
+      ['h', 'e', 'llo'],
+      ['n', 'e', 'w world'],
+    ]);
+    expect(divider(['hello', 'new world'], 5, 3)).toEqual([
+      ['hel', 'lo'],
+      ['new', ' w', 'orld'],
+    ]);
+  });
+
+  test('divide with string', () => {
+    expect(divider(['hello', 'new world'], 'e')).toEqual([
+      ['h', 'llo'],
+      ['n', 'w world'],
+    ]);
+    expect(divider(['hello', 'new world'], 'l')).toEqual([
+      ['he', 'o'],
+      ['new wor', 'd'],
+    ]);
+    expect(divider(['hello', 'new world'], 'a')).toEqual([
+      ['hello'],
+      ['new world'],
+    ]);
+  });
+
+  test('divide with multiple character separators', () => {
+    expect(divider(['hello', 'new world'], 'l', 'o')).toEqual([
+      ['he'],
+      ['new w', 'r', 'd'],
+    ]);
+    expect(divider(['hello', 'new world'], ' ', 'o')).toEqual([
+      ['hell'],
+      ['new', 'w', 'rld'],
+    ]);
+  });
+
+  test('divide with mixed numbers and characters', () => {
+    expect(divider(['hello', 'new world'], 2, 'l')).toEqual([
+      ['he', 'o'],
+      ['ne', 'w wor', 'd'],
+    ]);
+    expect(divider(['hello', 'new world'], ' ', 3)).toEqual([
+      ['hel', 'lo'],
+      ['new', 'world'],
+    ]);
+  });
+
+  test('edge cases', () => {
+    expect(divider([''], 'a')).toEqual([[]]);
+    expect(divider(['hello'], 0)).toEqual([['hello']]);
+    expect(divider(['hello'], 5)).toEqual([['hello']]);
+    expect(divider(['hello'], 10)).toEqual([['hello']]);
   });
 });
