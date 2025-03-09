0c350ef92c9d788dacac09a8dbcaafc4f652c39a
nyaomaru <nyaonyao0725@gmail.com>
2025-03-09 18:54:04 +0900

    fix: regex key

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -2,13 +2,17 @@ const regexCache = new Map<string, RegExp>();
 
 export function getRegex(separators: string[]): RegExp | null {
   if (separators.length === 0) return null;
-  const key = separators.join('');
+
+  const key = JSON.stringify(separators);
+
   if (!regexCache.has(key)) {
-    regexCache.set(
-      key,
-      new RegExp(`[${separators.map(escapeRegExp).join('')}]`, 'g')
+    const pattern = separators.reduce(
+      (acc, sep) => acc + escapeRegExp(sep),
+      ''
     );
+    regexCache.set(key, new RegExp(`[${pattern}]`, 'g'));
   }
+
   return regexCache.get(key)!;
 }
 
