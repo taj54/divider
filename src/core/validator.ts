2ac2c7cd74eb3c7539f80b3d40a3d630f398c04c
nyaomaru <nyaonyao0725@gmail.com>
2025-03-22 19:54:38 +0900

    fix: remove magic number

--- src/core/validator.ts
+++ src/core/validator.ts
@@ -1,3 +1,7 @@
 export function isOptions(arg: unknown): arg is { flatten?: boolean } {
   return typeof arg === 'object' && arg !== null && 'flatten' in arg;
 }
+
+export function isEmptyArray<T>(input: T[]): boolean {
+  return input.length === 0;
+}
