7f54ec02749af25cb6f321100cba5a5713a7b9cc
nyaomaru <nyaonyao0725@gmail.com>
2025-05-31 21:25:07 +0900

    fix: unused constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,12 +1,5 @@
 export const DividerExcludeModes = {
   NONE: 'none',
   EMPTY: 'empty',
-  WHITESPACE: 'whitespace'
+  WHITESPACE: 'whitespace',
 } as const;
-
-export const DividerErrorCodes = {
-  INVALID_INPUT: 'INVALID_INPUT',
-  INVALID_SEPARATOR: 'INVALID_SEPARATOR',
-  INVALID_OPTIONS: 'INVALID_OPTIONS',
-  INVALID_SIZE: 'INVALID_SIZE'
-} as const; 
